2f03403c8b2401a332fdb4ff52ffe585dd83a30a
2005leeyj <lyj_1018@yonsei.ac.kr>
2021-05-27 03:04:34 +0900

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Blog
+from .forms import BlogForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def first(request):
@@ -8,39 +10,48 @@ def first(request):
     count = Blog.objects.all().count()
     return render(request, 'first.html', {'blogs':blogs, 'count':count})
 
+@login_required(login_url='/account/login/')
 def home(request):
     blogs = Blog.objects.all ()
     return render(request,'home.html',{'blogs':blogs})
 
+@login_required(login_url='/account/login/')
 def detail(request, id):
     blog = get_object_or_404(Blog, pk = id)
     return render(request, 'detail.html', {'blog':blog})
 
+@login_required(login_url='/account/login/')
 def new(request):
-    return render(request, 'new.html')
+    form = BlogForm()
+    return render(request, 'new.html',{'form':form})
 
+@login_required(login_url='/account/login/')
 def create(request):
-    new_blog = Blog()
-    new_blog.title = request.POST['title']
-    new_blog.writer = request.POST['writer']
-    new_blog.body = request.POST['body']
-    new_blog.pud_date = timezone.now()
-    new_blog.save()
-    return redirect ('detail',new_blog.id)
+    form = BlogForm(request.POST, request.FILES)
+    if form.is_valid():
+        new_blog = form.save(commit=False)
+        new_blog.pud_date = timezone.now()
+        new_blog.save()
+        return redirect ('detail',new_blog.id)
+    return redirect('first')
 
+@login_required(login_url='/account/login/')
 def edit(request, id):
     edit_blog = Blog.objects.get(id=id)
     return render(request, 'edit.html',{'blog':edit_blog})
 
+@login_required(login_url='/account/login/')
 def update(request, id):
     update_blog = Blog.objects.get(id=id)
     update_blog.title = request.POST['title']
     update_blog.writer = request.POST['writer']
     update_blog.body = request.POST['body']
     update_blog.pud_date = timezone.now()
+    update_blog.image = request.FILES['image']
     update_blog.save()
     return redirect ('detail',update_blog.id)    
 
+@login_required(login_url='/account/login/')
 def delete(request, id):
   delete_blog = Blog.objects.get(id= id)
   delete_blog.delete()
--- lionproject/urls.py
+++ lionproject/urls.py
@@ -14,16 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import *
+from django.urls import path, include
+from blog.views import first
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',first,name="first"),
-    path('index/',home,name="home"),
-    path('<str:id>',detail,name="detail"),
-    path('new/',new,name="new"),
-    path('create/',create,name="create"),
-    path('edit/<str:id>',edit,name="edit"),
-    path('update/<str:id>',update,name="update"),
-]
+    path('blog/', include('blog.urls')),
+    path('account/', include('account.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
